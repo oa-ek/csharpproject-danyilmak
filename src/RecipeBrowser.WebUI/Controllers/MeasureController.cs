8ffbf4533a271bdb3d2524e58a76316cdd2fa882
DanyilMak <danyil.maksymchuk@oa.edu.ua>
2024-06-22 18:24:41 +0300

    feat: add admin Measure CRUD

---
+++ src/RecipeBrowser.WebUI/Controllers/MeasureController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeBrowser.Core.Entities;
+using RecipeBrowser.Repos.Common;
+
+namespace RecipeBrowser.WebUI.Controllers
+{
+    public class MeasureController : Controller
+    {
+        private readonly IRepository<Measure, Guid> _measureRepository;
+        public MeasureController(IRepository<Measure, Guid> measureRepository)
+        {
+            _measureRepository = measureRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _measureRepository.GetAllAsync());
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Measure model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _measureRepository.CreateAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            return View(await _measureRepository.GetAsync(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, Measure updatedModel)
+        {
+            try
+            {
+                await _measureRepository.UpdateAsync(updatedModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _measureRepository.DeleteAsync(id);
+                return Json(new { success = true, message = "Запис успішно видалено" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = $"Помилка при видаленні запису: {ex.Message}" });
+            }
+        }
+    }
+}
---
+++ src/RecipeBrowser.WebUI/Controllers/ProductTypeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipeBrowser.Core.Entities;
+using RecipeBrowser.Repos.Common;
+
+namespace RecipeBrowser.WebUI.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        private readonly IRepository<ProductType, Guid> _productTypeRepository;
+        public ProductTypeController(IRepository<ProductType, Guid> productTypeRepository)
+        {
+            _productTypeRepository = productTypeRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _productTypeRepository.GetAllAsync());
+        }
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductType model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _productTypeRepository.CreateAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            return View(await _productTypeRepository.GetAsync(id));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(Guid id, ProductType updatedModel)
+        {
+            try
+            {
+                await _productTypeRepository.UpdateAsync(updatedModel);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            try
+            {
+                await _productTypeRepository.DeleteAsync(id);
+                return Json(new { success = true, message = "Запис успішно видалено" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = $"Помилка при видаленні запису: {ex.Message}" });
+            }
+        }
+    }
+}
